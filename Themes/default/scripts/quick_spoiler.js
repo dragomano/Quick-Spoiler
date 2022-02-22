61d424e9483f94c55065501f752e7f9a46273869
Bugo <bugo@dragomano.ru>
2022-02-22 08:13:53 +0500

    Update quick_spoiler.js

--- Themes/default/scripts/quick_spoiler.js
+++ Themes/default/scripts/quick_spoiler.js
@@ -1,38 +1,42 @@
+/*eslint quotes: ["error", "single"]*/
+/*eslint-env es6*/
+
 jQuery(document).ready(function($) {
-    $('.sp-body img').each(addLoader);
+	let addLoader = function() {
+		$(this).attr({
+			'data-src': $(this).attr('src'),
+			src: smf_default_theme_url + '/images/loading_sm.gif'
+		});
+	}
 
-    const previewBody = document.getElementById('preview_body');
-    if (previewBody) {
-        const config = {childList: true};
-        const callback = function(mutationsList) {
-            for (let mutation of mutationsList) {
-                if (mutation.type === 'childList') {
-                    $('.sp-body img').each(addLoader);
-                }
-            }
-        };
+	$('.sp-body img').each(addLoader);
 
-        const observer = new MutationObserver(callback);
-        observer.observe(previewBody, config);
-    }
+	const previewBody = document.getElementById('preview_body');
 
-    $('body').on('click', '.sp-head', function() {
-        $this = $(this);
-        $this.parent().is('open') ? $this.next().slideUp('fast') : $this.next().slideDown('fast');
-        $this.parent().children('.sp-body').find('img').each(function() {
-            $(this).attr('src', $(this).attr('data-src'));
-        });
-    });
+	if (previewBody) {
+		const config = {childList: true};
+		const callback = function(mutationsList) {
+			for (let mutation of mutationsList) {
+				if (mutation.type === 'childList') {
+					$('.sp-body img').each(addLoader);
+				}
+			}
+		};
 
-    $('body').on('click', '.sp-foot', function() {
-        $this = $(this).parent();
-        $this.parent().removeAttr("open");
-    });
+		const observer = new MutationObserver(callback);
+		observer.observe(previewBody, config);
+	}
+
+	$('body').on('click', '.sp-head', function() {
+		$this = $(this);
+		$this.parent().is('open') ? $this.next().slideUp('fast') : $this.next().slideDown('fast');
+		$this.parent().children('.sp-body').find('img').each(function() {
+			$(this).attr('src', $(this).attr('data-src'));
+		});
+	});
+
+	$('body').on('click', '.sp-foot', function() {
+		$this = $(this).parent();
+		$this.parent().removeAttr("open");
+	});
 });
-
-let addLoader = function() {
-    $(this).attr({
-        'data-src': $(this).attr('src'),
-        src: smf_default_theme_url + '/images/loading_sm.gif'
-    });
-}
