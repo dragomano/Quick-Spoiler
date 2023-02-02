5596c6f3947b9c8449555cc4bef421aa576839ef
Bugo <cheetah@bk.ru>
2023-02-02 21:27:32 +0500

    Add Portuguese_pt translation

---
+++ Themes/default/languages/QuickSpoiler/.portuguese_pt.php
@@ -0,0 +1,26 @@
+<?php
+
+$txt['quick_spoiler'] = 'Spoiler';
+$txt['qs_title'] = 'Título padrão da tag';
+$txt['qs_settings'] = 'Configurações de spoiler';
+$txt['qs_no_spoiler_sorry'] = 'Lamentamos, mas não tem permissão para ver o conteúdo do spoiler.';
+$txt['groups_view_spoiler'] = 'Grupos que podem ver o conteúdo de spoilers';
+
+$txt['qs_footer']  = '[fechar]';
+$txt['qs_bgcolor'] = 'Cor do spoiler';
+$txt['qs_colors']  = array(
+	'default'   => 'Padrão',
+	'green'     => 'Verde',
+	'red'       => 'Vermelho',
+	'blue'      => 'Azul',
+	'yellow'    => 'Amarelo',
+	'orange'    => 'Cor de laranja',
+	'brown'     => 'Castanho',
+	'purple'    => 'Roxo',
+	'cyan'      => 'Azul claro',
+	'lime'      => 'Verde lima',
+	'steelblue' => 'Azul aço'
+);
+
+$txt['permissionname_view_spoiler'] = $txt['group_perms_name_view_spoiler'] = $txt['groups_view_spoiler'];
+$txt['permissionhelp_view_spoiler'] = 'Utilizadores podem ver informação escondida pelos spoilers.';
