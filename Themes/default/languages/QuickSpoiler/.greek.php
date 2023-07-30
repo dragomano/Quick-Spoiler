26ce039025dc7cf8bcbf8adb677a1895c1682730
Bugo <cheetah@bk.ru>
2023-07-30 14:51:28 +0500

    Add Greek translation

---
+++ Themes/default/languages/QuickSpoiler/.greek.php
@@ -0,0 +1,26 @@
+<?php
+
+$txt['quick_spoiler']       = 'Σπόιλερ';
+$txt['qs_title']            = 'Προεπιλεγμένος τίτλος σπόιλερ';
+$txt['qs_settings']         = 'Ρυθμίσεις σπόιλερ [Γρήγορο σπόιλερ]';
+$txt['qs_no_spoiler_sorry'] = 'Λυπούμαστε, αλλά δεν επιτρέπεται να δείτε τα περιεχόμενα του spoiler.';
+$txt['groups_view_spoiler'] = 'Προβολή περιεχομένων σπόιλερ';
+
+$txt['qs_footer']  = '[Κλειστό]';
+$txt['qs_bgcolor'] = 'Σπόιλερ χρώματα';
+$txt['qs_colors']  = array(
+    'default'   => 'Προκαθορισμένο',
+    'green'     => 'Πράσινο',
+    'red'       => 'Κόκκινο',
+    'blue'      => 'Μπλέ',
+    'yellow'    => 'Κίτρινο',
+    'orange'    => 'Πορτοκαλί',
+    'brown'     => 'Καφέ',
+    'purple'    => 'Μώβ',
+    'cyan'      => 'Κυανό',
+    'lime'      => 'Λεμονί',
+    'steelblue' => 'Μπλέ μέταλ'
+);
+
+$txt['permissionname_view_spoiler'] = $txt['group_perms_name_view_spoiler'] = $txt['groups_view_spoiler'];
+$txt['permissionhelp_view_spoiler'] = 'Οι χρήστες μπορούν να δουν πληροφορίες που είναι κρυμμένες κάτω από σπόιλερ.';
