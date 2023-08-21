0b3468b61765b76e42c311b2d25e94f12d15ae59
Bugo <cheetah@bk.ru>
2023-08-21 18:46:58 +0500

    Update Italian translation

--- Themes/default/languages/QuickSpoiler/.italian.php
+++ Themes/default/languages/QuickSpoiler/.italian.php
@@ -4,7 +4,23 @@ $txt['quick_spoiler'] = 'Spoiler';
 $txt['qs_title'] = 'Titolo spoiler di default';
 $txt['qs_settings'] = 'Impostazioni Spoiler [Spoiler Rapido]';
 $txt['qs_no_spoiler_sorry'] = 'Spiacente, ma non sei autorizzato a visualizzare il contenuto degli spoiler.';
-$txt['groups_view_spoiler'] = 'Visualizzare il contenuto degli spoiler';
+$txt['groups_view_spoiler'] = 'Visualizzazione contenuto degli spoiler';
+
+$txt['qs_footer']  = '[chiudi]';
+$txt['qs_bgcolor'] = 'Colore spoiler';
+$txt['qs_colors']  = array(
+	'default'   => 'Default',
+	'green'     => 'Verde',
+	'red'       => 'Rosso',
+	'blue'      => 'Blu',
+	'yellow'    => 'Giallo',
+	'orange'    => 'Arancione',
+	'brown'     => 'Marrone',
+	'purple'    => 'Viola',
+	'cyan'      => 'Ciano',
+	'lime'      => 'Lime',
+	'steelblue' => 'Blue acciaio'
+);
 
 $txt['permissionname_view_spoiler'] = $txt['group_perms_name_view_spoiler'] = $txt['groups_view_spoiler'];
 $txt['permissionhelp_view_spoiler'] = 'Gli utenti possono vedere il contenuto nascosto negli spoiler.';
