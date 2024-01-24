a801e85c23bd6190639f610dca79a80327403e85
Bugo <cheetah@bk.ru>
2024-01-24 15:35:55 +0500

    Update version

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # Quick Spoiler
+
 [![SMF 2.1](https://img.shields.io/badge/SMF-2.1-ed6033.svg?style=flat)](https://github.com/SimpleMachines/SMF2.1)
 ![License](https://img.shields.io/github/license/dragomano/quick-spoiler)
 ![Hooks only: Yes](https://img.shields.io/badge/Hooks%20only-YES-blue)
 
-* **Tested on:** PHP 7.4.30
-* **Languages:** Bulgarian, English, French, Italian, Polish, Portuguese, Russian, Spanish, Turkish, German, Greek
+- **Tested on:** PHP 8.0.30
+- **Languages:** Bulgarian, English, French, Italian, Polish, Portuguese, Russian, Spanish, Turkish, German, Greek
 
 ## Description
+
 Simple and beautiful jQuery spoiler, with custom title. Based on the [BBSpoiler](http://www.wordpressplugins.ru/posts/bbspoiler.html) plugin for Wordpress.
 
 ## Описание
+
 Красивый jQuery спойлер, как на Rutracker.org, на основе плагина [BBSpoiler](http://www.wordpressplugins.ru/posts/bbspoiler.html) для Wordpress.
 
 В комплекте несколько стилей оформления и подгрузка картинок только при открытии спойлера.
--- Sources/Class-QuickSpoiler.php
+++ Sources/Class-QuickSpoiler.php
@@ -6,28 +6,29 @@
  * @package Quick Spoiler
  * @link https://custom.simplemachines.org/mods/index.php?mod=2940
  * @author Bugo https://dragomano.ru/mods/quick-spoiler
- * @copyright 2011-2022 Bugo
+ * @copyright 2011-2024 Bugo
  * @license https://opensource.org/licenses/BSD-3-Clause BSD
  *
- * @version 1.5
+ * @version 1.5.4
  */
 
-if (!defined('SMF'))
-	die('Hacking attempt...');
+if (! defined('SMF'))
+	die('No direct access...');
 
 final class QuickSpoiler
 {
-	public function hooks()
+	public function hooks(): void
 	{
 		add_integration_function('integrate_load_theme', __CLASS__ . '::loadTheme#', false, __FILE__);
 		add_integration_function('integrate_load_permissions', __CLASS__ . '::loadPermissions#', false, __FILE__);
 		add_integration_function('integrate_bbc_codes', __CLASS__ . '::bbcCodes#', false, __FILE__);
 		add_integration_function('integrate_bbc_buttons', __CLASS__ . '::bbcButtons#', false, __FILE__);
+		add_integration_function('integrate_optimus_teaser', __CLASS__ . '::optimusTeaser#', false, __FILE__);
 		add_integration_function('integrate_prepare_display_context', __CLASS__ . '::prepareDisplayContext#', false, __FILE__);
 		add_integration_function('integrate_general_mod_settings', __CLASS__ . '::generalModSettings#', false, __FILE__);
 	}
 
-	public function loadTheme()
+	public function loadTheme(): void
 	{
 		global $context;
 
@@ -48,20 +49,20 @@ final class QuickSpoiler
 			text-align: left;
 		}');
 
-		if (!in_array($context['current_action'], array('helpadmin', 'printpage')))
-			loadJavaScriptFile('quick_spoiler.js', array('minimize' => true));
+		if (! in_array($context['current_action'], ['helpadmin', 'printpage']))
+			loadJavaScriptFile('quick_spoiler.js', ['minimize' => true]);
 	}
 
-	public function loadPermissions(array &$permissionGroups, array &$permissionList)
+	public function loadPermissions(array &$permissionGroups, array &$permissionList): void
 	{
-		$permissionList['membergroup']['view_spoiler'] = array(false, 'general', 'view_basic_info');
+		$permissionList['membergroup']['view_spoiler'] = [false, 'general', 'view_basic_info'];
 	}
 
-	public function bbcCodes(array &$codes)
+	public function bbcCodes(array &$codes): void
 	{
 		global $sourcedir, $modSettings, $txt;
 
-		if (!function_exists('allowedTo'))
+		if (! function_exists('allowedTo'))
 			require_once($sourcedir . '/Security.php');
 
 		loadLanguage('QuickSpoiler/');
@@ -74,77 +75,85 @@ final class QuickSpoiler
 
 		// Our spoiler tag
 		if (allowedTo('view_spoiler')) {
-			$codes[] = array(
+			$codes[] = [
 				'tag'         => 'spoiler',
-				'before'      => '<details class="sp-wrap sp-wrap-' . $style . '"><summary class="sp-head">' . (!empty($modSettings['qs_title']) ? $modSettings['qs_title'] : $txt['quick_spoiler']) . '</summary><div class="sp-body">',
+				'before'      => '<details class="sp-wrap sp-wrap-' . $style . '"><summary class="sp-head">' . (! empty($modSettings['qs_title']) ? $modSettings['qs_title'] : $txt['quick_spoiler']) . '</summary><div class="sp-body">',
 				'after'       => '<div class="sp-foot">' . $txt['qs_footer'] . '</div></div></details>',
-				'block_level' => true
-			);
-			$codes[] = array(
+				'block_level' => true,
+			];
+			$codes[] = [
 				'tag'         => 'spoiler',
 				'type'        => 'parsed_equals',
 				'before'      => '<details class="sp-wrap sp-wrap-' . $style . '"><summary class="sp-head">$1</summary><div class="sp-body">',
 				'after'       => '<div class="sp-foot">' . $txt['qs_footer'] . '</div></div></details>',
-				'block_level' => true
-			);
+				'block_level' => true,
+			];
 		} else {
-			$codes[] = array(
-				'tag'      => 'spoiler',
-				'type'     => 'unparsed_content',
-				'content'  => '<div class="errorbox centertext">' . $txt['qs_no_spoiler_sorry'] . '</div>',
-				'validate' => function (&$tag, &$data) {
+			$codes[] = [
+				'tag'              => 'spoiler',
+				'type'             => 'unparsed_content',
+				'content'          => '<div class="errorbox centertext">' . $txt['qs_no_spoiler_sorry'] . '</div>',
+				'validate'         => function (&$tag, $data) {
 					unset($data);
 				},
-				'disabled_content' => '<div class="errorbox centertext">' . $txt['qs_no_spoiler_sorry'] . '</div>'
-			);
-			$codes[] = array(
-				'tag'      => 'spoiler',
-				'type'     => 'unparsed_equals_content',
-				'content'  => '<div class="errorbox centertext">' . $txt['qs_no_spoiler_sorry'] . '</div>',
-				'validate' => function (&$tag, &$data) {
+				'disabled_content' => '<div class="errorbox centertext">' . $txt['qs_no_spoiler_sorry'] . '</div>',
+			];
+			$codes[] = [
+				'tag'              => 'spoiler',
+				'type'             => 'unparsed_equals_content',
+				'content'          => '<div class="errorbox centertext">' . $txt['qs_no_spoiler_sorry'] . '</div>',
+				'validate'         => function (&$tag, $data) {
 					unset($data);
 				},
-				'disabled_content' => '<div class="errorbox centertext">' . $txt['qs_no_spoiler_sorry'] . '</div>'
-			);
+				'disabled_content' => '<div class="errorbox centertext">' . $txt['qs_no_spoiler_sorry'] . '</div>',
+			];
 		}
 	}
 
-	public function bbcButtons(array &$buttons)
+	public function bbcButtons(array &$buttons): void
 	{
 		global $txt;
 
 		if (allowedTo('view_spoiler')) {
-			$buttons[count($buttons) - 1][] = array(
+			$buttons[count($buttons) - 1][] = [
 				'image'       => 'spoiler',
 				'code'        => 'spoiler',
 				'before'      => '[spoiler]',
 				'after'       => '[/spoiler]',
-				'description' => $txt['quick_spoiler']
-			);
+				'description' => $txt['quick_spoiler'],
+			];
 		}
 	}
 
-	public function prepareDisplayContext(array &$output)
+	public function optimusTeaser(array &$replacements): void
+	{
+		global $txt;
+
+		$replacements[$txt['quick_spoiler']] = '';
+		$replacements[$txt['qs_footer']] = '';
+	}
+
+	public function prepareDisplayContext(array &$output): void
 	{
 		if (allowedTo('view_spoiler'))
 			return;
 
 		if (strpos($output['body'], '[/spoiler]') !== false)
-			$output['body'] = strtr($output['body'], array('[/spoiler]' => ''));
+			$output['body'] = strtr($output['body'], ['[/spoiler]' => '']);
 	}
 
-	public function generalModSettings(array &$config_vars)
+	public function generalModSettings(array &$config_vars): void
 	{
 		global $modSettings, $txt;
 
 		if (isset($config_vars[0]))
-			$config_vars[] = array('title', 'qs_settings');
+			$config_vars[] = ['title', 'qs_settings'];
 
 		if (empty($modSettings['qs_title']))
-			updateSettings(array('qs_title' => $txt['quick_spoiler']));
+			updateSettings(['qs_title' => $txt['quick_spoiler']]);
 
-		$config_vars[] = array('text', 'qs_title');
-		$config_vars[] = array('select', 'qs_bgcolor', $txt['qs_colors']);
-		$config_vars[] = array('permissions', 'view_spoiler');
+		$config_vars[] = ['text', 'qs_title'];
+		$config_vars[] = ['select', 'qs_bgcolor', $txt['qs_colors']];
+		$config_vars[] = ['permissions', 'view_spoiler'];
 	}
 }
